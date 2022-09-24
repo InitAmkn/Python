a8dc7b1b0e191fa7d6ac93424c52511bfc7954cb
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2022-09-24 16:49:43 +0300

    Home_work_1-task 2 completed

--- Home_work_1/Task_2.py
+++ Home_work_1/Task_2.py
@@ -1,28 +1,57 @@
-'''Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Пример:
-
-- 6 -> да
-- 7 -> да
-- 1 -> нет
-
-'''
 '''
 Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
 '''
 
-'''
-Пример:
 
-- x=34; y=-30 -> 4
-- x=2; y=4-> 1
-- x=-34; y=-30 -> 3
-Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+def conjunctio(a, b):  # a ⋀ b
+    if a == 0 and b == 0:
+        return 0
+    if a == 0 and b == 1:
+        return 0
+    if a == 1 and b == 0:
+        return 0
+    if a == 1 and b == 1:
+        return 1
 
-Пример:
 
-- A (3,6); B (2,1) -> 5,09
-- A (7,-5); B (1,-1) -> 7,21
-'''
+def disjunctio(a, b):  # a ⋁ b
+    if a == 0 and b == 0:
+        return 0
+    if a == 0 and b == 1:
+        return 1
+    if a == 1 and b == 0:
+        return 1
+    if a == 1 and b == 1:
+        return 1
+
+
+def inversio(a):    # ¬ a
+    if a == 1:
+        return 0
+    if a == 0:
+        return 1
+
+
+def verification_of_approval(x, y, z):  # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
+    left_value = inversio(disjunctio(disjunctio(x, y), z))
+    right_value = conjunctio(conjunctio(inversio(x), inversio(y)), inversio(z))
+    if left_value == right_value:
+        return left_value
+    else:
+        return None
+
+
+def verification_of_approval_all_value(x, y, z):
+    i = 0
+    while i < 8:
+        print(f"¬({x[i]} ⋁ {y[i]} ⋁ {z[i]}) = {verification_of_approval(x[i], y[i], z[i])} and ",
+              f"¬{x[i]} ⋀ ¬{y[i]} ⋀ ¬{z[i]} = {verification_of_approval(x[i], y[i], z[i])}")
+        i += 1
+
+
+x = [0, 1, 0, 1, 0, 1, 0, 1]
+y = [0, 0, 1, 1, 0, 0, 1, 1]
+z = [0, 0, 0, 0, 1, 1, 1, 1]
+
+verification_of_approval_all_value(x, y, z)
